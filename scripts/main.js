57b9b1c0b16d33e937007a548b846ea6748f62b5
Simon Tallmyr <simon@tallmyr.se>
2022-11-28 22:46:04 +0000

    Get Values

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+function updateRangeValue(newVal) {
+    if (newVal <= 6) {
+        document.getElementById("length_text").color = "red"
+    }
+    document.getElementById("length_text").innerHTML = newVal
+
+}
+
+function generatePassword(){
+    let letters = document.getElementById("Letters").checked
+    let numbers = document.getElementById("Numbers").checked
+    let symbols = document.getElementById("Symbols").checked
+    let similar = document.getElementById("Similar").checked
+
+
+    
+}
